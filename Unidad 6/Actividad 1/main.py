f55767a953a6ef8e732a44034f547ba26c962c64
joasasso <joasasso@gmail.com>
2024-10-18 21:17:01 -0300

    Camino recursivo DFS funcional

--- Unidad 6/Actividad 1/main.py
+++ Unidad 6/Actividad 1/main.py
@@ -1,14 +1,23 @@
 from moduloGrafoSecuencial import grafoSecuencial
+import time
 
 def main():
-   g = grafoSecuencial(5)
+   g = grafoSecuencial(10)
    g.agregarArista(0, 1)
    g.agregarArista(1, 2)
    g.agregarArista(1, 3)
    g.agregarArista(2, 4)
+   g.agregarArista(3, 5)
+   g.agregarArista(4, 6)
+   g.agregarArista(5, 7)
+   g.agregarArista(6, 8)
+   g.agregarArista(7, 9)
+   g.agregarArista(0,9)
    
-   g.imprimirGrafo()
-   print(g.adyacentes(0))
-   print(g.camino(0, 4, []))
+   inicio = time.time()
+   print(g.hayarCamino(3, 8))
+   print(f"El grafo es conexo: {g.conexo()}")
+   fin = time.time()
+   print(f"Tiempo de ejecución: {fin - inicio}")
 if __name__ == "__main__":
    main()
--- Unidad 6/Actividad 1/moduloGrafoSecuencial.py
+++ Unidad 6/Actividad 1/moduloGrafoSecuencial.py
@@ -1,10 +1,12 @@
 class grafoSecuencial:
    __matriz : list[list[int]]
    __cantNodos : int
+   __visitados : list[int]
    
    def __init__(self, cantNodos: int):
       self.__cantNodos = cantNodos
       self.__matriz = []
+      self.__visitados = []
       for i in range(0, cantNodos):
          self.__matriz.append([0] * cantNodos)
    
@@ -27,22 +29,37 @@ class grafoSecuencial:
             adyacentes.append(i)
       return adyacentes
    
-   def camino(self, nodoActual, nodoFin, camino) -> list:
-      camino.append(nodoActual)
+   def camino(self, nodoActual, nodoFin, caminoActual) -> list:
+      caminoActual.append(nodoActual)
+      self.__visitados.append(nodoActual)
       if nodoActual == nodoFin:
-         return camino
+         return caminoActual
       adyacentes = self.adyacentes(nodoActual)
-      if nodoFin in adyacentes:
-         camino.append(nodoFin)
-         return camino
+      if nodoFin in adyacentes :
+         caminoActual.append(nodoFin)
+         return caminoActual
       else:
-         caminoFinal = camino
+         caminoFinal = [1] * (self.__cantNodos + 1)
          for a in adyacentes:
-            if a not in camino:
-               nuevoCamino = self.camino(a, nodoFin, camino)
+            if a not in self.__visitados:
+               nuevoCamino = self.camino(a, nodoFin, caminoActual.copy())
                if len(nuevoCamino) < len(caminoFinal):
                   caminoFinal = nuevoCamino
          return caminoFinal
 
+   def hayarCamino(self, nodoInicio, nodoFin) -> list:
+      caminoActual = []
+      self.__visitados = []
+      camino = self.camino(nodoInicio, nodoFin, caminoActual)
+      if camino == [1] * (self.__cantNodos + 1):
+         camino = []
+      return camino
    
+   def conexo(self):
+      contador = 1
+      for i in range(1, self.__cantNodos):
+         camino = self.hayarCamino(0, i)
+         if camino != []:
+            contador += 1
+      return contador == self.__cantNodos
    
