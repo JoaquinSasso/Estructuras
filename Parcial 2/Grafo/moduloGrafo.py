4ea59b082b62873edba7a343b872327e14bdd14d
joasasso <joasasso@gmail.com>
2024-10-30 21:48:56 -0300

    Se agregaron comentarios a la busqueda de camino

--- Parcial 2/Grafo/moduloGrafo.py
+++ Parcial 2/Grafo/moduloGrafo.py
@@ -22,13 +22,13 @@ class Grafo:
    def camino(self, vActual, vFin, visitados = set(), recorrido = []): #Encuentra el camino entre vActual y vFin de manera recursiva y considerando ciclos
       visitados.add(vActual)
       recorrido.append(vActual)
-      if vActual == vFin and self.__matriz[vActual][vFin] != 0:
+      if vActual == vFin and self.__matriz[vActual][vFin] != 0: #Si se intenta ir desde un vertice a el mismo y hay loop
          return recorrido
       adyacentes = self.adyacentes(vActual)
-      if vFin in adyacentes:
+      if vFin in adyacentes:  #Si el vertice final es adyacente del actual
          recorrido.append(vFin)
          return recorrido
       else:
          for ady in adyacentes:
-            if ady not in visitados:
-               return self.camino(ady, vFin, visitados, recorrido.copy())
+            if ady not in visitados: #Se evitan los nodos que ya han sido visitados
+               return self.camino(ady, vFin, visitados, recorrido.copy()) #Se analizan todos los adyacentes del actual que todavia no hayan sido visitados
